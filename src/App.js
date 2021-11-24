0d06cc19c836f378bf39b8f4b7082092b996aa3f
Adrien26-source! <adrien.cher7@gmail.com>
2021-11-24 19:30:15 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.png';
 import './App.css';
+import Profile from './Profile';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
 function App() {
   return (
+    
+      <Router basename={process.env.PUBLIC_URL}>
+     
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </header>
+               <Switch>
+                    <Route exact path="/" render={() => (
+                        <Redirect to="/profilelist"/>
+                    )}/>
+                    <Route exact path='/profilelist' component={Profile}/>
+                </Switch>
+
+            </div>
+        </Router>
+    );    
 }
 
 export default App;
---
+++ src/Square.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Square extends React.Component {
+  
+    render() {
+      return (
+        <div>{this.props.text}
+        <button
+          className="square"        onClick={() => this.setState({value: 'X'})}      >
+          {this.state.value}      </button>
+          </div>
+      );
+    }
+  }
